006260b3ae0affb7131f684ae33a48ea4fbc5896
Rafael Mohlin <raffe_mohlin@hotmail.com>
2016-09-22 16:54:21 +0200

    Remove log

--- ssh/.ssh/get_agent.sh
+++ ssh/.ssh/get_agent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 new_agent() {
-    echo "Creating new ssh-agent"
+    #echo "Creating new ssh-agent"
     ssh-agent | sed 's/^echo/#echo/' > "$HOME/.ssh/agent"
     source "$HOME/.ssh/agent"
     ssh-add
@@ -10,10 +10,10 @@ new_agent() {
 if [ -s "$HOME/.ssh/agent" ]; then
     SSH_AGENT_PID=$(grep "^SSH_AGENT_PID=" "$HOME/.ssh/agent" | cut -d= -f2 | cut -d\; -f1)
     if kill -0 $SSH_AGENT_PID 1>/dev/null 2>&1; then
-        echo "Reusing ssh-agent"
+        #echo "Reusing ssh-agent"
         source "$HOME/.ssh/agent"
     else
-        echo "Dead ssh-agent"
+        #echo "Dead ssh-agent"
         rm "$HOME/.ssh/agent"
         new_agent
     fi
